416471850442774b769766dfb1e9a44708bf2d1c
Edson Henrique Baraldi <72748677+BaraldiM@users.noreply.github.com>
2023-07-04 16:22:38 -0300

    Variáveis
    Ajustes de variáveis

--- .idea/Altera bloco k.py
+++ .idea/Altera bloco k.py
@@ -6,18 +6,19 @@ from tkinter import filedialog
 def selecionar_arquivo():
     # Exibir caixa de diálogo para selecionar o arquivo
     caminho_arquivo = filedialog.askopenfilename()
+    print(caminho_arquivo)
 
     # Verificar se o arquivo foi selecionado
     if caminho_arquivo:
-        entry_caminho_arquivo.delete(0, tk.END)
-        entry_caminho_arquivo.insert(tk.END, caminho_arquivo)
-        entry_caminho_arquivo.config(state="readonly")
+        entrada_caminho.delete(0, tk.END)
+        entrada_caminho.insert(tk.END, caminho_arquivo)
+        #entry_caminho_arquivo.config(state="readonly")
 
 def substituir_data():
     # Obter os valores inseridos nos campos de entrada
-    data_antiga = entry_data_antiga.get()
-    data_nova = entry_data_nova.get()
-    caminho_arquivo = entry_caminho_arquivo.get()
+    data_antiga = entrada_data_antiga.get()
+    data_nova = entrada_data_nova.get()
+    caminho_arquivo = entrada_caminho.get()
 
     # Abra o arquivo em modo de leitura
     with open(caminho_arquivo, 'r') as arquivo:
@@ -46,20 +47,20 @@ janela = tk.Tk()
 janela.title("Trocar datas bloco K")
 janela.geometry("400x200")
 
-entry_caminho_arquivo = tk.Entry(janela, state="normal")
-entry_caminho_arquivo.pack()
+entrada_caminho = tk.Entry(janela, state="normal")
+entrada_caminho.pack()
 button_selecionar_arquivo = tk.Button(janela, text="Selecionar arquivo", command=selecionar_arquivo)
 button_selecionar_arquivo.pack()
 
 label_data_antiga = tk.Label(janela, text="Data antiga:")
 label_data_antiga.pack()
-entry_data_antiga = tk.Entry(janela)
-entry_data_antiga.pack()
+entrada_data_antiga = tk.Entry(janela)
+entrada_data_antiga.pack()
 
 label_data_nova = tk.Label(janela, text="Data nova:")
 label_data_nova.pack()
-entry_data_nova = tk.Entry(janela)
-entry_data_nova.pack()
+entrada_data_nova = tk.Entry(janela)
+entrada_data_nova.pack()
 
 # Cria um botão para substituir a data
 botao_substituir = tk.Button(janela, text="Substituir", command=substituir_data)
